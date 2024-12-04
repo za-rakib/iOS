8accd01a9b218f2c55e4ac1c86af509d01e8541a
Rakib <rakib@sayburgh.com>
2024-12-04 15:39:05 +0600

    chat view title set

--- Flash-Chat-2/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-2/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -16,6 +16,8 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
         
     }
     
