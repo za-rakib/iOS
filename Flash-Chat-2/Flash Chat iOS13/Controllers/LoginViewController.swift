dfc228a8c6ed096268c00240e720f2d58b2736a1
Md Rakib Hossen <rakib.dev70@gmail.com>
2024-12-06 23:49:54 +0600

    segue name set dynamically

--- Flash-Chat-2/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash-Chat-2/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -23,7 +23,7 @@ class LoginViewController: UIViewController {
                     print(e.localizedDescription)
                 }
                 else{
-                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
                 }
             }
         }
